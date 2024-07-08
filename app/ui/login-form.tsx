178689b28332a9fcc402403a71ec9227d64eaa52
Arturo Quiroz <r2inlondon@gmail.com>
2024-07-08 20:24:44 +0100

    style : fixed the sytle for the login form

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -4,7 +4,7 @@ export default function LoginForm() {
   return (
     <form action={getLogin} className="my-6 w-full md:my-8">
       <input
-        className="text-md my-6 block w-full rounded-sm border-gray-500 py-2 pl-2 shadow-md focus:shadow-lg focus:shadow-green-900 focus:outline-none focus:ring-2 focus:ring-green-200 focus:ring-offset-2 md:my-8 md:text-xl"
+        className="text-md my-4 w-full rounded-sm border-gray-500 py-2 pl-2 shadow-md focus:shadow-lg focus:shadow-green-900 focus:outline-none focus:ring-2 focus:ring-green-200 focus:ring-offset-2 md:text-xl"
         type="text"
         placeholder="Email address"
         name="email"
@@ -12,7 +12,7 @@ export default function LoginForm() {
       />
 
       <input
-        className="text-md my-6 block w-full rounded-md border-gray-500 py-2 pl-2 shadow-md focus:border-indigo-500 focus:shadow-lg focus:shadow-green-900 focus:outline-none focus:ring-2 focus:ring-green-200 focus:ring-offset-2 md:my-8 md:text-xl"
+        className="text-md my-4 w-full rounded-md border-gray-500 py-2 pl-2 shadow-md focus:border-indigo-500 focus:shadow-lg focus:shadow-green-900 focus:outline-none focus:ring-2 focus:ring-green-200 focus:ring-offset-2 md:text-xl"
         type="password"
         autoComplete="new-password"
         placeholder="Password"
