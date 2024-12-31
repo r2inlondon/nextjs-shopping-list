1eaa7969b192a195d01192853d6eabc292cafba2
Arturo <r2inlondon@gmail.com>
2024-12-30 22:02:14 -0600

    feat: root page has a default redirection to /login in place now

---
+++ app/login/page.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+import LoginForm from "../ui/login-form";
+
+export default function Home() {
+  return (
+    <div className="flex h-screen w-full items-center">
+      <div className="md:w-550 m-auto w-full bg-white p-2.5 md:p-5">
+        <LoginForm />
+        <div>
+          <span className="text-md text-txt-pri-color md:text-xl">
+            Need an account?
+          </span>
+          <Link href="/register">
+            <span className="text-md px-2 text-blue-600 md:text-xl">
+              SignUp
+            </span>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,5 @@
-import Link from "next/link";
+import { redirect } from 'next/navigation'
 
-import LoginForm from "./ui/login-form";
-
-export default function Home() {
-  return (
-    <div className="flex h-screen w-full items-center">
-      <div className="md:w-550 m-auto w-full bg-white p-2.5 md:p-5">
-        <LoginForm />
-        <div>
-          <span className="text-md text-txt-pri-color md:text-xl">
-            Need an account?
-          </span>
-          <Link href="/register">
-            <span className="text-md px-2 text-blue-600 md:text-xl">
-              SignUp
-            </span>
-          </Link>
-        </div>
-      </div>
-    </div>
-  );
+export default function Page() {
+  redirect('/login')
 }
